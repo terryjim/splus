ec3306d435e1c2e4c3f7126a80de85d76fbe4d1e
terryjim <terryjim@163.com>
2018-01-24 17:27:18 +0800

    init

--- src/main/java/cn/com/topit/splus/SplusApplication.java
+++ src/main/java/cn/com/topit/splus/SplusApplication.java
@@ -1,12 +1,28 @@
 package cn.com.topit.splus;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @SpringBootApplication
 public class SplusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplusApplication.class, args);
 	}
+	 @RequestMapping("/")
+		public String hello(Locale locale, Model model) {
+			/*model.addAttribute("greeting", "Hello!");
+
+			Date date = new Date();
+			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);        
+			String formattedDate = dateFormat.format(date);
+			model.addAttribute("currentTime", formattedDate);
+*/
+			return "index";
+		}
 }
