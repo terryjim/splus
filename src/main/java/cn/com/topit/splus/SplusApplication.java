05c31c19242f3e094255f1f91f9b8d4df4a0016c
terryjim <terryjim@163.com>
2018-01-24 11:21:56 +0800

    init

---
+++ src/main/java/cn/com/topit/splus/SplusApplication.java
@@ -0,0 +1,12 @@
+package cn.com.topit.splus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplusApplication.class, args);
+	}
+}
